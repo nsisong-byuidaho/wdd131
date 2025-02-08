6d386a7487258490fc33b1c94cf443830511bc4a
nsisong-byui <akp24023@byui.edu>
2025-02-08 12:10:47 +0100

    fixed bom page

---
+++ week05/scripts/bom.js
@@ -0,0 +1,58 @@
+const b = document.querySelector("button");
+const input = document.querySelector("#favchap");
+const ul = document.querySelector("#list");
+
+let chaptersArray = getChapterList() || [];
+
+
+chaptersArray.forEach(chapter => displayList(chapter));
+
+
+b.addEventListener("click", () => {
+    if (input.value != "")
+    {
+        displayList(input.value);
+        chaptersArray.push(input.value);
+        setChapterList();
+
+        input.value = "";
+        input.focus();    
+    }
+    else {
+        input.focus();
+    }
+});
+
+
+
+function displayList(item) {
+    const li = document.createElement("li");
+    const del = document.createElement("button"); 
+    li.textContent = item;
+    del.textContent = "❌";
+    del.classList.add("delete");
+    
+    li.appendChild(del);
+    ul.appendChild(li);
+    
+    del.addEventListener("click", () => {
+            ul.removeChild(li);
+            deleteChapter(li.textContent);
+            input.focus();
+    });
+}
+
+function setChapterList() {
+    localStorage.setItem("BOMList", JSON.stringify(chaptersArray));
+}
+
+function getChapterList() {
+    return JSON.parse(localStorage.getItem("BOMList"));
+}
+
+function deleteChapter(chapter) {
+    chapter = chapter.slice(0, chapter.length -1);
+    chaptersArray = chaptersArray.filter(item => item !== chapter);
+    setChapterList();
+}
+
